d5a0b9f7c9d3be0792a8144fc946628444cb30d2
IRivera99 <113114@tecnicatura.frc.utn.edu.ar>
2022-10-05 21:47:48 -0300

    Final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BlackjackCartasComponent } from './blackjack/blackjack-cartas/blackjack-cartas.component';
 import { BlackjackJuegoComponent } from './blackjack/blackjack-juego/blackjack-juego.component';
 import { ErrorComponent } from './error/error.component';
 import { HomeComponent } from './home/home.component';
@@ -11,7 +10,8 @@ const routes: Routes = [
   {path: 'login', component: InicioSesionComponent},
   {path: 'home', component: HomeComponent},
   {path: 'juego', component: BlackjackJuegoComponent},
-  {path: '**', component: HomeComponent}
+  {path: '**', component: ErrorComponent},
+  {path: '', component: HomeComponent}
 ];
 
 @NgModule({
